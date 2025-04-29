954c196c575145b5ab278336db273ef6c429cca0
PHẠM ĐÌNH ANH TUẤN <143309309+phamdinhanhtuan@users.noreply.github.com>
2025-04-29 23:14:30 +0700

    bubbleSort

---
+++ onTap1.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Hoán đổi arr[j] và arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Mang sau khi sap xep:\n");
+    printArray(arr, n);
+
+    return 0;
+}
